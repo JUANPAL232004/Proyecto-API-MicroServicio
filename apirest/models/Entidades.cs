8b5cd46cdf92b60002c386e71c7a150d901d4777
Juan Pablo <juannnnpablolopez@gmail.com>
2026-02-01 00:28:41 -0500

    Limpieza: eliminando bin/ y obj/ y configurando gitignore

---
+++ apirest/models/Entidades.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apirest.models
+{
+    public class Entidades
+    {
+        public int 
+    }
+    
+    public class Producto
+    {
+     public int IdProducto { get;set; }
+     public string NombreProducto { get;set; }   
+     public string Cliente { get;set; }
+     public decimal Precio { get;set; }
+     public int Stock { get;set; }
+     public int IdEstado { get;set; }
+     public DateTime { get;set; }
+    }
+
+    public class Usuario
+    {
+        
+    }
+
+    public class Estado
+    {
+        
+    }
+}
