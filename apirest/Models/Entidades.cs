71e4365c30fc16c5bf55c74c88d7afd1688c251a
Juan Pablo <juannnnpablolopez@gmail.com>
2026-02-01 04:58:42 -0500

    Agregación libreria coockies, para autenticación login y register

--- apirest/Models/Entidades.cs
+++ apirest/Models/Entidades.cs
@@ -1,27 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace apirest.Models
 {
     public class Producto
     {
         public int IdProducto { get; set; }
-        public required string NombreProducto { get; set; }   
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Nombre demasiado corto o largo")]
+        [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "No se permiten caracteres especiales")]
+        public required string NombreProducto { get; set; }
+
+        [Required]
         public required string Cliente { get; set; }
+
+        [Range(0.01, 1000000, ErrorMessage = "El precio debe ser mayor a 0")]
         public decimal Precio { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
         public int Stock { get; set; }
+
         public int IdEstado { get; set; }
-        public DateTime Fecha { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Fecha { get; set; } = DateTime.Now;
     }
 
     public class Usuario
-    {
+    {   
         public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "Nombre de usuario obligatorio")]
+        [StringLength(100, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "No se permiten caracteres especiales")]
         public required string NombreUsuario { get; set; }
+        
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres")]
+        [DataType(DataType.Password)]
         public required string Password { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de correo inválido")]
+        [StringLength(150)]
         public required string CorreoElectronico { get; set; }
     }
 
--- apirest/Program.cs
+++ apirest/Program.cs
@@ -1,5 +1,6 @@
 using apirest.interfaces;
 using apirest.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IEstadoRepository, EstadoRepository>();
 builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login"; // A dónde redirigir si no está logueado
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(20); // Duración de la sesión
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
 
 var app = builder.Build();
 
